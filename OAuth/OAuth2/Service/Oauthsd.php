7f7d2b60cb8b18334aa3299db302ccd979e050d5
bdegoy <consultants@degoy.com>
2019-06-14 10:50:19 +0200

    //dnc4 state is required by OAuthSD

--- OAuth/OAuth2/Service/Oauthsd.php
+++ OAuth/OAuth2/Service/Oauthsd.php
@@ -1,6 +1,16 @@
 <?php
-//dnc3
-  
+/**  
+* OpenID Connect Authentication with Oauthsd
+* @link     https://oa.dnc.global
+* Oauthsd.php OAuth service for the Lusitanian PHPoAuthLib 
+* @link     https://github.com/Lusitanian/PHPoAuthLib
+* @author   Bertrand Degoy bertrand@degoy.com
+* @copyright (c) 2018 B.Degoy DnC https://degoy.com
+* @license  http://www.opensource.org/licenses/mit-license.html MIT License
+*/ 
+
+//dnc4
+
 namespace OAuth\OAuth2\Service;
 
 use OAuth\OAuth2\Token\StdOAuth2Token;
@@ -12,18 +22,15 @@ use OAuth\Common\Storage\TokenStorageInterface;
 use OAuth\Common\Http\Uri\UriInterface;
 
 
-/** OpenID Connect Authentication with OAuthSD
-* @link https://oa.dnc.global
-* @author Bertrand Degoy bertrand@degoy.com
-* @copyright (c) 2018 B.Degoy DnC https://degoy.com
-*/
 class Oauthsd extends AbstractService
 {
+
     /**
-     * Defined scopes
-     * @link https://oa.dnc.global/-Sujets-communs-.html#definitionetgestiondesscopesdansoauthsd
-     * @see  #attributes
-     */
+    * Available scopes (will be tested by AbstractService). 
+    * Might be larger than those defined by client application.
+    * @link https://oa.dnc.global/-Sujets-communs-.html#definitionetgestiondesscopesdansOAuthsd
+    * @see  #attributes
+    */
     const SCOPE_OPENID           = 'openid';
     const SCOPE_PROFILE          = 'profile';
     const SCOPE_EMAIL            = 'email';
@@ -38,6 +45,9 @@ class Oauthsd extends AbstractService
         $scopes = array(),
         UriInterface $baseApiUri = null
     ) {
+
+        $scopes = array_merge($scopes, array(openid, sli, profile));   // profile ???
+
         parent::__construct($credentials, $httpClient, $storage, $scopes, $baseApiUri);
 
         if (null === $baseApiUri) {
@@ -46,32 +56,65 @@ class Oauthsd extends AbstractService
     }
 
     /**
-     * {@inheritdoc}
-     */
+    * Override abstract function in order to provide required parameters in authorization request.
+    * State is required by OAuthSD
+    * Scopes :
+    * openid is required by OpenID Connect, sli is particular to OAuthSD, basic is enough for phpBB.
+    * @link https://oa.dnc.global/-Sujets-communs-.html#definitionetgestiondesscopesdansOAuthsd
+    */
+    public function getAuthorizationUri(array $additionalParameters = array())
+    {
+        $parameters = array_merge(
+            $additionalParameters,
+            array(
+                //'type'          => 'web_server',
+                'client_id'     => $this->credentials->getConsumerId(),
+                'redirect_uri'  => $this->credentials->getCallbackUrl(),
+                'response_type' => 'code',
+                'scope'         => 'openid sli',    // do not mention basic.       
+            )
+        );
+
+        if (!isset($parameters['state'])) {
+            $parameters['state'] = $this->generateAuthorizationState();
+        }
+        $this->storeAuthorizationState($parameters['state']);
+
+        // Build the url
+        $url = clone $this->getAuthorizationEndpoint();
+        foreach ($parameters as $key => $val) {
+            $url->addToQuery($key, $val);
+        }
+        return $url;
+    }
+
+    /**
+    * {@inheritdoc}
+    */
     public function getAuthorizationEndpoint()
     {
         return new Uri('https://oa.dnc.global/authorize');
     }
 
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public function getAccessTokenEndpoint()
     {
         return new Uri('https://oa.dnc.global/token');
     }
 
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     protected function getAuthorizationMethod()
     {
-        return static::AUTHORIZATION_METHOD_HEADER_BEARER;
+        return static::AUTHORIZATION_METHOD_HEADER_BEARER;        // ou AUTHORIZATION_METHOD_QUERY_STRING ???
     }
 
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     protected function parseAccessTokenResponse($responseBody)
     {
         $data = json_decode($responseBody, true);
@@ -100,5 +143,33 @@ class Oauthsd extends AbstractService
 
         return $token;
     }
-}
 
+    /**
+    * {@inheritdoc}
+    */
+    public function requestAccessToken($code, $state = null)
+    {
+        if (null !== $state) {
+            $this->validateAuthorizationState($state);
+        }
+
+        $bodyParams = array(
+            'code'          => $code,
+            'client_id'     => $this->credentials->getConsumerId(),
+            'client_secret' => $this->credentials->getConsumerSecret(),
+            'redirect_uri'  => $this->credentials->getCallbackUrl(),
+            'grant_type'    => 'authorization_code',
+        );
+
+        $responseBody = $this->httpClient->retrieveResponse(
+            $this->getAccessTokenEndpoint(),
+            $bodyParams,
+            $this->getExtraOAuthHeaders()
+        );
+        
+        $token = $this->parseAccessTokenResponse($responseBody);
+        $this->storage->storeAccessToken($this->service(), $token);
+
+        return $token;
+    }
+}
